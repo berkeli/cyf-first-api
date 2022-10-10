f1b7a8823536086f8f4d012098b4442fd3ee09c6
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-10-10 11:19:34 +0100

    changed my IP address to localhost

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 let YOUR_NAME = "ekemini"
 let YOUR_LUCKY_NUMBER = "3"
 
-let URL = "http://13.40.212.192:5001"
+let URL = "http://localhost:5001"
 
 app.get('/', (req, res) => {       
     res.send(`Welcome to ${YOUR_NAME}'s 1st API`);                                                        
